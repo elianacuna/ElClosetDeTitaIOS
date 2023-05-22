536c7587012d54cf34087b74535309dc9cf5bf1b
Elian Acuña <eacunap@miumg.edu.gt>
2023-05-21 21:48:47 -0600

    Initial Commit

---
+++ ElClosetDeTitaIOS/ElClosetDeTitaIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElClosetDeTitaIOSApp.swift
+//  ElClosetDeTitaIOS
+//
+//  Created by Elian Acuña on 21/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ElClosetDeTitaIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
